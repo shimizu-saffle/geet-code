c34fa78fe2df1d259b6b0ffcd2e34a2869bfa90f
shimizu-saffle <shimizu.saffle@gmail.com>
2023-04-10 07:13:07 +0900

    refactor: addByIndex dart

--- problem_3/problem_3.dart
+++ problem_3/problem_3.dart
@@ -1,11 +1,9 @@
 void main() {
-  List<int> addByIndex(List<int> listA, List<int> listB) {
-    final sumList = <int>[];
-    for (var i = 0; i < listA.length; i++) {
-      sumList.add(listA[i] + listB[i]);
-    }
-    return sumList;
-  }
+  List<int> addByIndex(List<int> listA, List<int> listB) => listA
+      .asMap()
+      .entries
+      .map((entry) => entry.value + listB[entry.key])
+      .toList();
 
   const listA = [1, 2, 3];
   const listB = [4, 5, 6];
