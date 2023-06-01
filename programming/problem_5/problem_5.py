0b0bb022cf3c5d31f5bc1073e06a865016b348fa
shimizu-saffle <shimizu.saffle@gmail.com>
2023-06-01 21:14:33 +0900

    fix: expand_around_center py

--- programming/problem_5/problem_5.py
+++ programming/problem_5/problem_5.py
@@ -1,7 +1,20 @@
 from typing import List
 
-def expand_around_center(string: str, left_index: int, right_index: int) -> tuple:    
+def expand_around_center(string: str, left_index: int, right_index: int) -> tuple: 
+    # 左右のインデックスが有効な範囲内にあるか確認
+    if left_index < 0 or len(string) <= right_index:
+        return left_index, right_index
+    # 左右のインデックスを境界内で展開し、回文である限り続ける
     while left_index >= 0 and right_index < len(string) and string[left_index] == string[right_index]:
         left_index -= 1
         right_index += 1
-    return left_index + 1, right_index + 1
+    # 最終的な左右のインデックスを返す
+    return left_index + 1, right_index - 1
+
+print(
+    expand_around_center(
+        'abacdcbeebdbefaafbefdbbeabcdeedcbaebdcacba',
+        29,
+        28,
+    )
+)
