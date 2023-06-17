ccfa09a269ba778a7708ec74c33ce2ae4496f510
shimizu-saffle <shimizu.saffle@gmail.com>
2023-06-17 09:00:27 +0900

    find_longest_palindrome_substring rb

--- programming/problem_5/problem_5.rb
+++ programming/problem_5/problem_5.rb
@@ -11,4 +11,22 @@ def expanded_around_center(string, left_index, right_index)
    return [left_index + 1, right_index - 1]
 end
 
-puts expanded_around_center('abacdcbeebdbefaafbefdbbeabcdeedcbaebdcacba', 28, 29)
+def find_longest_palindrome_substring(string)
+   longest = ""
+
+   for i in 0..(2 * string.length - 1) 
+      left = i / 2
+      right = left + i % 2
+
+      left, right = expanded_around_center(string, left, right)
+
+      if right - left + 1 > longest.length
+         longest = string[left..right]
+      end      
+   end
+
+
+   longest
+end
+
+puts find_longest_palindrome_substring('abacdcbeebdbefaafbefdbbeabcdeedcbaebdcacba')
