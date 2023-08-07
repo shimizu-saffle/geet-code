6d9b11a100a1e5d59426235a1d992f7012b80c17
shimizu-saffle <shimizu.saffle@gmail.com>
2023-08-08 08:28:12 +0900

    refactor: improve answer

--- programming/problem_6/problem_6.dart
+++ programming/problem_6/problem_6.dart
@@ -1,23 +1,14 @@
 void main() {
   print(
-    average([1, 2, 3, 4, 5]),
+    averageExcludingMinMax([1, 2, 3, 4, 5]),
   );
 }
 
-double average(List<int> numbers) {
-  var min = numbers.first;
-  var max = numbers.last;
-  for (var n in numbers) {
-    if (n < min) {
-      min = n;
-    } else if (n > max) max = n;
-  }
-  // 最大値と最小値を除く
-  print('min: $min');
-  numbers.remove(min);
-  numbers.remove(max);
-  // 平均値を求める
-  // 要素数で合計値を割る
-  print('numbers: $numbers');
-  return numbers.reduce((a, b) => a + b) / numbers.length;
+double averageExcludingMinMax(List<int> inputNumbers) {
+  final min = inputNumbers.reduce((curr, next) => curr < next ? curr : next);
+  final max = inputNumbers.reduce((curr, next) => curr > next ? curr : next);
+  inputNumbers
+    ..removeWhere((e) => e == min)
+    ..removeWhere((e) => e == max);
+  return inputNumbers.reduce((a, b) => a + b) / inputNumbers.length;
 }
