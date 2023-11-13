21768eeb758ae8f80961fe2ef6eef09ea54f3f11
shimizu-saffle <shimizu.saffle@gmail.com>
2023-11-13 21:24:36 +0900

    delete rust directory

--- programming/rust/src/problem_4.rs
+++
@@ -1,7 +0,0 @@
-fn is_all_unique(input: &str) -> bool{
-    input.chars().collect::<std::collections::HashSet<_>>().len() == input.len()
-}
-
-fn main() {
-    println!("{}", is_all_unique("abc"));
-}
--- programming/rust/target/debug/deps/rust_project-9740ddef22fb6b8b.d
+++
@@ -1,5 +0,0 @@
-/Users/kentashimizu/development/geet-code/rust_project/target/debug/deps/rust_project-9740ddef22fb6b8b.rmeta: src/main.rs
-
-/Users/kentashimizu/development/geet-code/rust_project/target/debug/deps/rust_project-9740ddef22fb6b8b.d: src/main.rs
-
-src/main.rs:
